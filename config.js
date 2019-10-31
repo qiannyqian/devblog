eb52939f8235ef4affd712bfb7ad220dd9ee6dc4
Hui Qian <huiqian@jobkred.com>
2019-11-01 00:33:23 +0800

    changed colour, removed header image

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ module.exports = {
   title: 'A Gander At Gatsby', // Required
   author: 'qiannyqian', // Required
   description: 'Front-End Web Developer',
-  primaryColor: '#3498db', // Required
-  showHeaderImage: true,
+  primaryColor: '#9E1946', // Required
+  showHeaderImage: false,
   showShareButtons: true,
   postsPerPage: 3, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
+    website: 'https://devblog-11.netlify.com',
     github: 'https://github.com/qiannyqian',
     twitter: 'https://twitter.com/qiannyqianhere',
     linkedin: 'https://sg.linkedin.com/in/huiqianliang',
